5d526873a7ff7f4f6012691cfa87cf5997419078
Simon RENOULT <simon.renoult@gmail.com>
2014-06-15 14:55:19 +0200

    [FIX] Syntax

--- routes/api.js
+++ routes/api.js
@@ -6,93 +6,90 @@ var models = require('../models')
 // Routes
 // =====================================
 
-module.exports = function() {
+router
 
-	router
+	// USERS
+	// ===========================================
 
-		// USERS
-		// ===========================================
+	// Show the user list as JSON
+	.get('/users', function (req, res) {
+		models.User.findAll().success(function(users) {
+			res.end(JSON.stringify(users));
+		});
+	})
 
-		// Show the user list as JSON
-		.get('/users', function (req, res) {
-			models.User.findAll().success(function(users) {
-				res.end(JSON.stringify(users));
+	// Add a new user
+	.post('/users', function(req, res) {
+		res.end('TODO');
+	})
+
+	// Delete all the users
+	.delete('/users', function (req, res) {
+		models.User.findAll().success(function(users) {
+			users.forEach(function(user) {
+				user.destroy();
 			});
-		})
+		});
 
-		// Add a new user
-		.post('/users', function(req, res) {
-			res.end('TODO');
-		})
+		res.end();
+	})
 
-		// Delete all the users
-		.delete('/users', function (req, res) {
-			models.User.findAll().success(function(users) {
-				users.forEach(function(user) {
-					user.destroy();
-				});
+	// Show a single user as JSON
+	.get('/users/:id', function (req, res) {
+		models.User.find(req.params.id).success(function(user) {
+			res.end(JSON.stringify(user));
+		});
+	})
+
+	// LANGUAGES
+	// ==========================================
+
+	// Show the language list as JSON
+	.get('/languages', function(req, res) {
+		models.Language.findAll().success(function(languages) {
+			res.end(JSON.stringify(languages));
+		});
+	})
+
+	// Add a new language
+	.post('/languages', function(req, res) {
+		models.Language.create(req.body).success(function(language){
+			res.status(201).end(JSON.stringify(language));
+		})
+	})
+
+	.delete('/languages', function (req, res) {
+		models.Language.findAll().success(function(languages) {				languages.forEach(function(language) {
+				language.destroy();
 			});
+		});
 
-			res.end();
-		})
+		res.end();
+	})
 
-		// Show a single user as JSON
-		.get('/users/:id', function (req, res) {
-			models.User.find(req.params.id).success(function(user) {
-				res.end(JSON.stringify(user));
+	// MESSAGES
+	// ===========================================
+
+	.get('/messages', function(req, res) {
+		models.Message.findAll().success(function(messages) {
+			res.end(JSON.stringify(messages));
+		});
+	})
+
+	.post('/messages', function(req, res) {
+		models.Message.create(req.body).success(function(message) {
+			res.status(201).end(JSON.stringify(message));
+		});
+	})
+
+	.delete('/messages', function (req, res) {
+		models.Message.findAll().success(function(messages) {
+			messages.forEach(function(message) {
+				message.destroy();
 			});
-		})
+		});
 
-		// LANGUAGES
-		// ==========================================
+		res.end();
+	})
 
-		// Show the language list as JSON
-		.get('/languages', function(req, res) {
-			models.Language.findAll().success(function(languages) {
-				res.end(JSON.stringify(languages));
-			});
-		})
-
-		// Add a new language
-		.post('/languages', function(req, res) {
-			models.Language.create(req.body).success(function(language){
-				res.status(201).end(JSON.stringify(language));
-			})
-		})
-
-		.delete('/languages', function (req, res) {
-			models.Language.findAll().success(function(languages) {				languages.forEach(function(language) {
-					language.destroy();
-				});
-			});
-
-			res.end();
-		})
-
-		// MESSAGES
-		// ===========================================
-
-		.get('/messages', function(req, res) {
-			models.Message.findAll().success(function(messages) {
-				res.end(JSON.stringify(messages));
-			});
-		})
-
-		.post('/messages', function(req, res) {
-			models.Message.create(req.body).success(function(message) {
-				res.status(201).end(JSON.stringify(message));
-			});
-		})
-
-		.delete('/messages', function (req, res) {
-			models.Message.findAll().success(function(messages) {
-				messages.forEach(function(message) {
-					message.destroy();
-				});
-			});
-
-			res.end();
-		})
-
-	return router;
-};
+module.exports = router;
